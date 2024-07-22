b9a7ef341b2ee982fd7097a1bcfd2e98b932defd
BrandonTay_Legion <tayweiyong1007@gmail.com>
2024-07-22 22:47:41 +0800

    add print_width

--- utils/ft_print_flags.c
+++ utils/ft_print_flags.c
@@ -6,7 +6,7 @@
 /*   By: twei-yo- <twei-yo-@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 13:49:06 by twei-yo-          #+#    #+#             */
-/*   Updated: 2024/07/14 15:38:33 by twei-yo-         ###   ########.fr       */
+/*   Updated: 2024/07/19 19:08:06 by twei-yo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@ int	ft_pad_width(int width, char c)
 {
 	int	i;
 
-	if (width < 0)
-		return (0);
 	i = 0;
 	while (i < width)
 	{
@@ -26,10 +24,28 @@ int	ft_pad_width(int width, char c)
 	return (i);
 }
 
-
-
-/* int main(int argc, char const *argv[])
+int	print_width(struct format *s_f, int len, int nb)
 {
-	print_zero(17);
+	int	w_c;
+
+	w_c = 0;   
+	if (s_f->zero_pad && s_f->precision < 0)
+		w_c += ft_pad_width(s_f->width - len, '0');
+	else
+	{
+		if (s_f->precision >= len)
+			len = s_f->precision;
+		if ((nb < 0 || s_f->plus || s_f->space))
+			len++;
+		w_c += ft_pad_width(s_f->width - len, ' ');
+	}
+	return (w_c);
+}
+
+/* 
+int main(int argc, char const *argv[])
+{
+	int i = ft_pad_width(-10, '0');
+	printf("%i", i);
 	return 0;
 } */
